c52e72ffa9bd3ef9b379ce40890e4973f64162f2
petrhruza <petr.hruza@hotmail.cz>
2022-04-03 22:03:19 +0200

    Final navbar and hamburger styling + website adjustments. Ready to deploy.

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const hamburger = document.querySelector(
   ".header .navbar .nav-list .hamburger"
 );
 const mobileMenu = document.querySelector(".header .navbar .nav-list ul");
-const header = document.querySelector(".header .container");
+const header = document.querySelector(".header.container");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".navlink").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    mobileMenu.classList.remove("active");
+  })
+);
+
+document.addEventListener("scroll", () => {
+  let scrollPosition = window.scrollY;
+  if (scrollPosition > 150) {
+    header.style.backgroundColor = '#29323c';
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
 });
